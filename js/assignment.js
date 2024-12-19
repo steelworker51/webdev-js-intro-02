c14bc9f8917cd930b28fa1651e18397a7233ccbc
dale murphy <smcpsycho51@gmail.com>
2024-12-19 16:49:15 -0500

    ade changes

--- js/assignment.js
+++ js/assignment.js
@@ -3,28 +3,39 @@
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
 const yearElement = document.getElementById("year");
-const resolutionElement = document.getElementById("resolution")
-const willMeetResolutionElement = document.getElementById("will-meet-resolution")
-const submissionBtn = document.getElementById("submission-btn")
+const resolutionElement = document.getElementById("resolution");
+const willMeetElement = document.getElementById("will-meet-resolution");
+const submissionBtn = document.getElementById("submission-btn");
 
 // Declare your variables here.
+const currentYear = "2024";
+const resolution = "become better at programming, maybe change fields";
+const willMeet = "yes";
 
-function updateYear() {
-    // write the logic
-
+// Function to update the current year
+function updateCurrentYear() {
+    yearElement.innerText = currentYear;
 }
 
+// Function to update the resolution
 function updateResolution() {
-    // Update this function 
-
+    resolutionElement.innerText = resolution;
 }
 
+// Function to update whether the resolution will be met
+function updateWillMeetResolution() {
+    willMeetElement.innerText = willMeet;
+}
+
+// Function to render all updates
 function render() {
-    // Finish writing this function
-
+    updateCurrentYear();
+    updateResolution();
+    updateWillMeetResolution();
 }
 
+// Event listener for the submission button
 submissionBtn.addEventListener("click", function () {
-    // Finish writing this function
+    render();
+});
 
-})
